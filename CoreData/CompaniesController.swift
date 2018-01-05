969134493bb10f7939b383dc2168504cf979f068
Kilian Hiestermann <ian-h@gmx.net>
2018-01-05 10:29:14 +0100

    ready to integrate CoreData

---
+++ CoreData/CompaniesController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Kilian on 04.01.18.
+//  Copyright © 2018 Kilian. All rights reserved.
+//
+
+import UIKit
+
+class CompaniesController: UITableViewController, CreateCompanyControllerDelegate {
+    func didAddCompany(company: Company) {
+        let tesla = company
+        companies.append(tesla)
+        
+        let newIndexPath = IndexPath(row: companies.count - 1, section: 0)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+    }
+    
+    
+    var companies = [Company]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Companies"
+    
+
+        tableView.backgroundColor = .darkblue
+        tableView.tableFooterView = UIView()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        tableView.separatorColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        
+        
+    }
+    
+    @objc func handleAddCompany() {
+        let createCompanyController = CreateCompanyController()
+        
+        let navController = CustomNavigationController(rootViewController: createCompanyController)
+        
+        createCompanyController.delegate = self
+        
+        present(navController, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return companies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        
+        cell.backgroundColor = .tealColor
+    
+        let company = companies[indexPath.row]
+        cell.textLabel?.text = company.name
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        return cell
+    }
+
+}
+
---
+++ CoreData/CreateCompanyController.swift
@@ -0,0 +1,89 @@
+//
+//  CreateCompanyController.swift
+//  CoreData
+//
+//  Created by Kilian on 04.01.18.
+//  Copyright © 2018 Kilian. All rights reserved.
+//
+
+import UIKit
+
+protocol CreateCompanyControllerDelegate {
+    func didAddCompany(company: Company)
+}
+
+class CreateCompanyController: UIViewController {
+    
+    var delegate: CreateCompanyControllerDelegate?
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Name here"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Create Company"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave))
+        view.backgroundColor = .darkblue
+        
+        setupUI()
+    }
+    
+    @objc func handleSave() {
+//        dismiss(animated: true) {
+//            guard let name = self.nameTextField.text else { return }
+//
+//            let company = Company(name: name, founded: Date())
+//
+//            self.delegate?.didAddCompany(company: company)
+//        }
+    
+
+    }
+    
+    @objc func handleCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+  
+    private func setupUI() {
+        
+        let lightBlueBackgroundView = UIView()
+        lightBlueBackgroundView.backgroundColor = .lightBlue
+        lightBlueBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(lightBlueBackgroundView)
+        
+        lightBlueBackgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        lightBlueBackgroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        lightBlueBackgroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        lightBlueBackgroundView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameLabel)
+        
+        nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        nameLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameTextField)
+        nameTextField.leftAnchor.constraint(equalTo: nameLabel.rightAnchor).isActive = true
+        nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor)
+        nameTextField.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: nameLabel.topAnchor).isActive = true
+
+        
+    }
+}
